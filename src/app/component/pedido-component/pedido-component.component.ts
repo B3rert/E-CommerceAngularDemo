c45fa76a3d0033a54bdc6bd31b90938fe73ddc35
Robert L <robertmorales181@gmail.com>
2021-08-31 12:03:38 -0600

    Tabla legible en cualquier dispositivo (Solo la tabla falta el resto de la pagina)

--- src/app/component/pedido-component/pedido-component.component.ts
+++ src/app/component/pedido-component/pedido-component.component.ts
@@ -1,37 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 
-export interface PeriodicElement {
-  fecha: string;
-  pedido: string;
-  total: string;
-  estado: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {pedido: "18578", fecha: 'Julio 18, 2021', total: "Q.1.0079", estado: 'Cancelado'},
-  {pedido: "2272", fecha: 'Julio 18, 2021', total: "Q.4.0026", estado: 'Cancelado'},
-  {pedido: "725873", fecha: 'Julio 18, 2021', total: "Q.6.941", estado: 'Cancelado'},
-  {pedido: "472872872", fecha: 'Julio 18, 2021', total: "Q.9.0122", estado: 'Cancelado'},
-  {pedido: "57287", fecha: 'Julio 18, 2021', total: "Q.10.811", estado: 'Cancelado'},
-  {pedido: "67287", fecha: 'Julio 18, 2021', total: "Q.12.0107", estado: 'Cancelado'},
-  {pedido: "772727", fecha: 'Julio 18, 2021', total: "Q.14.0067", estado: 'Cancelado'},
-  {pedido: "89739", fecha: 'Julio 18, 2021', total: "Q.15.9994", estado: 'Cancelado'},
-  {pedido: "979347", fecha: 'Julio 18, 2021', total: "Q.18.9984", estado: 'Cancelado'},
-  {pedido: "107837", fecha: 'Julio 18, 2021', total: "Q.20.1797", estado: 'Cancelado'},
-];
 @Component({
   selector: 'app-pedido-component',
   templateUrl: './pedido-component.component.html',
   styleUrls: ['./pedido-component.component.css']
 })
+
 export class PedidoComponentComponent implements OnInit {
 
-  constructor() { }
+  faEye = faEye;
+
+  jsonHead: any[] = [];
+  jsonPedidos: any[] = [
+    {
+      "pedido": "4564as8",
+      "fecha": "25/08/2021",
+      "estado": "Cancelado",
+      "total": "Q.205.00",
+    },
+    {
+      "pedido": "275257",
+      "fecha": "25/08/2021",
+      "estado": "Cancelado",
+      "total": "Q.205.00"
+    },
+    {
+      "pedido": "728728",
+      "fecha": "25/08/2021",
+      "estado": "Cancelado",
+      "total": "Q.205.00"
+    },
+    {
+      "pedido": "72872",
+      "fecha": "25/08/2021",
+      "estado": "Cancelado",
+      "total": "Q.205.00"
+    },
+    {
+      "pedido": "2877",
+      "fecha": "25/08/2021",
+      "estado": "Cancelado",
+      "total": "Q.205.00"
+    },
+    {
+      "pedido": "78737777",
+      "fecha": "25/08/2021",
+      "estado": "Cancelado",
+      "total": "Q.205.00"
+    }
+  ];
+
+  constructor() {
+    this.resolveKeyJson(this.jsonPedidos[0]);
+  }
 
   ngOnInit(): void {
   }
 
-   displayedColumns: string[] = ['pedido', 'fecha', 'total', 'estado'];
-  dataSource = ELEMENT_DATA;
+  resolveKeyJson(objectJson: any) {
 
+    for (var key in objectJson) {
+
+      this.jsonHead.push(key);
+
+    }
+  }
+
+  viewDetails(pedido:any){
+    console.log(pedido);
+  }
 }

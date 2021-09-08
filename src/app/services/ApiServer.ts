cb1e3339b04547eb7b6f93b6acf01dd263dc288c
Robert L <robertmorales181@gmail.com>
2021-09-08 09:16:11 -0600

    Apis publicadas

--- src/app/services/ApiServer.ts
+++ src/app/services/ApiServer.ts
@@ -1,5 +1,6 @@
 export const URL_API = {
     ApiSqlServer:{
-        url: "https://localhost:44315/api/", //Alojamineto de apis
+        //url: "https://localhost:44315/api/", //Alojamineto de apis 
+        url: "http://b3rert.somee.com/api/", //Alojamineto de apis 
     }
 }

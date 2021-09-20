8305326aa00877f23de4cdcde0744cbef91b96bf
Robert L <robertmorales181@gmail.com>
2021-09-20 17:47:43 -0600

    Dev

--- src/app/services/ApiServer.ts
+++ src/app/services/ApiServer.ts
@@ -1,6 +1,6 @@
 export const URL_API = {
     ApiSqlServer: {
-        //url: "https://localhost:44315/api/", //Alojamineto de apis 
-         url: "https://b3rert.somee.com/api/", //Alojamineto de apis x
+        url: "https://localhost:44315/api/", //Alojamineto de apis 
+        // url: "https://b3rert.somee.com/api/", //Alojamineto de apis x
     }
 }

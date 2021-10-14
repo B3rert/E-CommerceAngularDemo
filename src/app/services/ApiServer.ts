2541f2ce24c05324af7ba51ffaaeb447ebe0a36b
Robert L <robertmorales181@gmail.com>
2021-10-14 08:51:19 -0600

    Cambios para pruebas de produccion

--- src/app/services/ApiServer.ts
+++ src/app/services/ApiServer.ts
@@ -4,8 +4,8 @@ let url = configuraciones.urlApis;
 
 export const URL_API = {
     ApiSqlServer: {
-       // url: "https://localhost:44315/api/", //Alojamineto de apis 
-        url: url, //Alojamineto de apis 
+        //url: "https://localhost:44315/api/", //Alojamineto de apis 
+       url: url, //Alojamineto de apis 
         // url: "https://b3rert.somee.com/api/", //Alojamineto de apis x
     }
 }

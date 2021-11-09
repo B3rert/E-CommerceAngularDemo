e23e26529d1274f939a0b66e35cd8a83a065d3b4
Robert L <robertmorales181@gmail.com>
2021-11-08 18:41:35 -0600

    Cambios en las apis, aquí no se relizaron cambios, sigue actualizar y eliminar pedidos.

--- src/app/interfaces/pedido.interface.ts
+++ src/app/interfaces/pedido.interface.ts
@@ -1,14 +1,21 @@
-export interface Pedido{
-    pedido:any[];
-    user:string;
-    tienda_pedido:any;
-    tipo_pedido:any;
+export interface Pedido {
+    pedido: any[];
+    user: string;
+    tienda_pedido: any;
+    tipo_pedido: any;
 }
 
-export interface GetDocumentoEstructura{
+export interface GetDocumentoEstructura {
     consecutivo_Interno: string;
     fecha_Hora: Date;
     tipo_Estructura: string;
     estructura: string;
     estado: number;
+}
+
+export interface DelPutDocumentoEstructura {
+    pConsecutivo_Interno: number
+    pEstructura: string
+    pUserName: string
+    pEstado: number
 }
